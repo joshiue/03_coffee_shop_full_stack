995470dbe14343a0ee2b86f3e3f48d4b23870fe0
joshiue <jaitonit@yahoo.co.uk>
2022-11-16 17:40:56 +0100

    update

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -133,7 +133,7 @@ def verify_decode_jwt(token):
             raise AuthError({'code': 'invalid_header',
                 'description': 'Unable to parse authentication token.'}, 400)
     raise AuthError({'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'}, 400)
+                'description': 'Unable to find the appropriate key.'}, 403)
 
 '''
 @TODO implement @requires_auth(permission) decorator method
